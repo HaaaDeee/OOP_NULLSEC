c7d101a706222fca9a3ee220c53f44829228292d
FAZuH <fazuhhh@proton.me>
2024-12-06 22:53:32 +0700

    feat!: add UI

--- nullsec/src/Manager/UI.cs
+++ nullsec/src/Manager/UI.cs
@@ -12,3 +12,141 @@ public class UI
         return 0;
     }
 }
+// [Intro]
+// Lonely rori-rori kami, kourin
+// Kane wo naraseba kono toori
+// Kore wa kyuusai no rekuiemu
+//
+// [Refrain]
+// See no
+// Sawattara taiho! (Ah!)
+// Goku chu de tenko! (Uh!)
+// Ichi, ni, san, shii? (Gomen nasaai)
+// Haa? Gomen nasai ga
+// Kikoenaii (Eh!)
+// Shukusei! Rori kami rekuiemu
+//
+// [Verse 1]
+// Shourai wa omae nanka ni
+// Sukaretakunai (Nai!)
+// Doromizu de kuchi wo arae
+// Houtei de aou? (Ne)
+// Saa omawari san
+// Tsurete itte
+// Mousou shika torie no nai
+// Kawaiso na hito
+//
+// [Pre-Chorus]
+// (Ui-chan, Ui-chan!)
+// (Matte yo Ui-chan!)
+// Sono sugata oya ni miseraremasu ka?
+// (Ui-chan, Ui-chan!)
+// (Kawaii yoo!)
+// Waa, arigatou
+// Otsukaresama desu
+// See upcoming pop shows
+// Get tickets for your favorite artists
+// You might also like
+// Say Don’t Go (Taylor’s Version) [From The Vault]
+// Taylor Swift
+// “Slut!” (Taylor’s Version) [From The Vault]
+// Taylor Swift
+// Is It Over Now? (Taylor’s Version) [From The Vault]
+// Taylor Swift
+// [Chorus]
+// Rori-rori kami, kourin (Zukkyuun!)
+// Maji yaba yatsura ni youjin (Bakkyuun!)
+// Matomo janai nara
+// Hayaku kiete kudasai
+// Lovely rori-rori kami, kourin (Zukyuun!)
+// Kane wo naraseba kono toori (Yes!)
+// Totemo yasashikute suteki na onii-chan
+// Rakuen de hora nenne shi na
+//
+// [Post-Chorus]
+// (Ui-chan, Ui-chan, Ui-chan, Ui-chan!)
+// (Ui-chan, Ui-chan, Ui-chan, Ui-chan!)
+// Hora, yono naka ni wa konna ni kimochi warui hitotachi ga
+// (Ui-chan, Ui-chan, Ui-chan, Ui-chan!)
+// Habikotte irun desu!
+// (Ui-chan, Ui, kocchi muitee)
+// Minna, sarashi agemasu yo
+//
+// [Refrain]
+// Sawattara taiho! (Ah!)
+// Goku chu de tenko! (Uh!)
+// Ichi, ni, san, shii? (Gomen nasaai)
+// Haa? Gomen nasai ga
+// Kikoenaii (Eh!)
+// Shukusei! Rori kami rekuiemu
+// [Interlude]
+// Haai, gokuchuu ni iru mina shaan
+// Keimusho fan miitingu no ojikan deesu
+// Sono, kusari kitta mimi wo
+// Gorigori kappojitte
+// Youuku kiite kudasai ne
+//
+// [Verse 2]
+// Zaa ko (Tasukaru)
+// Kimo kimo (Tasukaru)
+// Urusai (Tasukaru)
+// Damatte moratte ii desu ka? (Tasukaru)
+// Kirai desu (Tasukaru)
+// Kawaiso (Tasukaru)
+// Daasa yoowa mittomonaa
+// Doushiyou mo nai onii-chan onee-chan
+// Mou kibou mo nai gomi da omeera
+// Obutsu shoudoku hyakuto ban
+// Koyuu kekkai ha-tsu-do-u
+//
+// [Interlude]
+// Shukusei, Ui biimu (Ah~)
+// Mouitchou, Ui biimu (Ah~)
+// Mada madaa, Ui biimu (Ah~)
+// Ikari no hi
+// Sei naru intaanetto ni chi no ame ga furisosoida hi
+// (Ui biimu)
+// Orokana rorikon wo (Ui biimu)
+// Hone no zui made jouka suru hitosuji no hikari (Ui biimu)
+// Sore ga, Ui biimu nano de aru
+// "Ui wa, subete wo yurushimasu"
+// [Bridge]
+// Ah-ah, kono yo no minikusa wo
+// Atsumete egaite miru yo
+// Masshiro na kyanbasu ga
+// Kegarete naite iru
+// Lonely furimuita shoujo wa
+// Sotto te wo tsukamu
+// "Rakuen nado nai kara"
+//
+// [Interlude]
+// Okkina otomodachi no minna e
+// Omaera ga uchikonde iru kyomu ni wa
+// Nan no imi ga aru no?
+// Nee, oshiete yo
+// Ui, zenzenn wakannainda!
+//
+// [Chorus]
+// Rori-rori kami, kourin (Zukkyuun!)
+// Sayonara egao de baibai (Bakkyuun!)
+// Piriodo wo douzo kono te de oshite ageyu
+// Lovely rori-rori kami, kourin (Zukkyuun!)
+// Kane wo naraseba kono toori (Yes!)
+// Totemo yasashikute suteki na onii-chan
+// Rakuen de hora nenne shi na
+//
+// [Post-Chorus]
+// Sore ga kyuusai no
+// Rekuiemu
+//
+// [Refrain]
+// See no
+// Sawattara taiho! (Ah!)
+// Goku chu de tenko! (Uh!)
+// Ichi, ni, san, shii? (Gomen nasaai)
+// Haa? Gomen nasai ga
+// Kikoenaii (Eh!)
+// Shukusei! Rori kami rekuiemu
+//
+// [Outro]
+// Hontou ni, kimochi warui

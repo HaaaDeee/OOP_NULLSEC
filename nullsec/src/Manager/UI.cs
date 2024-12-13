e14c71968338970c90000feb220f08e9922cf625
HendhyW <hendhywirawan@gmail.com>
2024-12-14 00:23:29 +0700

    adding generic

--- nullsec/src/Manager/UI.cs
+++ nullsec/src/Manager/UI.cs
@@ -21,11 +21,29 @@ public class UI
         }
     }
 
-    public static int BattleManager(PlayerCharacter player, Enemy enemy)
+    public static int BattleManager<T>(PlayerCharacter player, T enemy) where T: Enemy
     {
-        Console.WriteLine("");
-        return 0;
+        Console.WriteLine("Enemy :");
+        Console.WriteLine(enemy.Faction);
+        Console.WriteLine("Health : " + enemy.Health + "/" + enemy.MaxHealth);
+        Console.WriteLine("============================================");
+        Console.WriteLine("Player: ");
+        Console.WriteLine(player.CharacterName);
+        Console.WriteLine("Health : " + player.Health + "/" + player.MaxHealth);
+        Console.WriteLine("============================================");
+        Console.WriteLine("Pilihan Aksi : ");
+        Console.WriteLine("1. Attack");
+        Console.WriteLine("2. Skill"); //skill (bisa di skip)
+        Console.WriteLine("3. Item");
+        Console.WriteLine("4. Kabur");
+        Console.WriteLine("============================================");
+        Console.WriteLine("Pilihan anda :");
+        string? choice = Console.ReadLine();
+        int int_choice;
+        Int32.TryParse(choice, out int_choice);
+        return int_choice;
     }
+}
     
     public static void NewGame() {
         // string[] line1 = {
